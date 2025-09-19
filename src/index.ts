fe2705d7d215298a484e2bec3fa928254e19e987
DreaTheEmpress <benc0007@videndjurs.net>
2025-09-19 11:15:13 +0200

    it starts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+import {Starter} from './Starter'
+import './style.scss'
+
+(()=> {
+
+    let language:string = "TypeScript";
+    let myVersion:number = 1.02;
+
+
+  const html = new Starter();
+
+ 
+})();
